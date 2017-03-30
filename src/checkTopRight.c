f0d0e95ae2208ed5b8a86f21ebde7e4d76fdeeab
marc brout <marc.brout@epitech.eu>
2017-03-30 17:12:45 +0100

    norme done

--- src/checkTopRight.c
+++ src/checkTopRight.c
@@ -5,7 +5,7 @@
 ** Login   <marc.brout@epitech.eu>
 **
 ** Started on  Tue Mar 28 19:01:14 2017 brout_m
-** Last update Tue Mar 28 19:02:06 2017 brout_m
+** Last update Thu Mar 30 17:05:07 2017 brout_m
 */
 
 #include "game.h"
@@ -19,7 +19,7 @@ int isTop(Player me, Map m)
 
 int isTopRight(Player me, Map m)
 {
-  if (me->y == 0 || me->x  == WIDTH - 1)
+  if (me->y == 0 || me->x == WIDTH - 1)
     return (-1);
   return (TOPR(me, m));
 }
